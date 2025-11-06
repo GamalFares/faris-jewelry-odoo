3dd437b76d7994de11a9537eb35e6c61e7b9681e
Gamal Fares <GamalFaresly@gmail.com>
2025-11-06 04:52:00 +0200

    Fix Odoo installation and Python path issues

--- start_odoo.sh
+++ start_odoo.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-set -e
-
-echo "=== Starting Faris Jewelry Odoo on Render ==="
-
-# Wait a bit for database to be ready
-sleep 5
-
-# Start Odoo
-exec python odoo/odoo-bin \
-    --addons-path=odoo/addons,custom-addons \
-    --database=faris_jewelry \
-    --db_host=${DB_HOST} \
-    --db_user=${DB_USER} \
-    --db_password=${DB_PASSWORD} \
-    --http-port=$PORT \
-    --without-demo=all \
-    --admin-passwd=admin123 \
-    --proxy-mode
+echo "Starting Odoo..."
+cd odoo
+export PYTHONPATH="/opt/render/project/src/odoo:$PYTHONPATH"
+exec python odoo-bin --addons-path=addons,../custom-addons --database=faris_jewelry --db_host=${DB_HOST} --db_user=${DB_USER} --db_password=${DB_PASSWORD} --http-port=${PORT} --without-demo=all --admin-passwd=admin123 --proxy-mode
